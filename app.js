cb73d5313647a8f70a03bc484ff3c19bbe6c9dfa
Mariam Grigorenko <109353093+MariamGrigorenko@users.noreply.github.com>
2022-12-20 21:21:58 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const request = require("request");
+const bodyParser = require("body-parser");
+const mailchimp = require("@mailchimp/mailchimp_marketing");
+// const path = require("path");
+const https = require("https");
+
+const app = express();
+
+// *** Body Parser ***
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// *** Static Folder ***
+app.use(express.static(__dirname + "/public"));
+// app.use("/public", express.static(path.join(__dirname, "public")));
+
+// *** Tracking HTML File ***
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+
+mailchimp.setConfig({
+  apiKey: "6fafd03c337748025a8d8d85f9a8506b-us11",
+  server: "us11",
+});
+
+// *** Signup Route ***
+app.post("/", function (req, res) {
+
+  const subscribingUser = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+  };
+
+  const listId = "a58491cbc1";
+
+  async function run() {
+    try {
+      const response = await mailchimp.lists.addListMember(listId, {
+        email_address: subscribingUser.email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: subscribingUser.firstName,
+          LNAME: subscribingUser.lastName,
+        },
+
+      });
+      console.log(response);
+      res.sendFile(__dirname + "/success.html");
+    } catch (error) {
+      console.log("Something went wrong.");
+      res.sendFile(__dirname + "/failure.html");
+    }
+  }
+  run();
+});
+
+app.post("/success.html", function(req, res) {
+  res.redirect("/");
+})
+
+app.post("/failure.html", function(req, res) {
+  res.redirect("/");
+})
+
+app.listen(process.env.PORT || 4000, function () {
+  console.log("Server started on port: 4000!");
+  });
